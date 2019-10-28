c0b4cf766e8c117bd9f3bb7d1a5b16a0fb8b0f32
Paris <parismollochristondis@gmail.com>
2019-10-28 19:13:37 +0100

    parsing

--- named_tuples.py
+++ named_tuples.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 import datetime
-
 StockPrice = namedtuple('StockPrice',
 ['symbol', 'date', 'closing_price'])
 
@@ -8,7 +7,7 @@ price = StockPrice('MSFT', datetime.date(2018, 12, 14), 106.03)
 assert price.symbol == 'MSFT'
 assert price.closing_price == 106.03
 
-from typing import NamedTuple
+from typing import NamedTuple, List
 
 class StockPrice(NamedTuple):
     symbol: str
@@ -22,3 +21,55 @@ price = StockPrice('MSFT', datetime.date(2018, 12, 14), 106.03)
 
 assert price.symbol == 'MSFT'
 assert price.closing_price == 106.03
+
+
+from dateutil.parser import parse
+
+def parse_row(row: List[str]) -> StockPrice:
+    symbol, date, closing_price = row
+    return StockPrice(symbol=symbol, date=parse(date).date(), closing_price=float(closing_price))
+
+stock = parse_row(["MSFT", "2018-12-14", "106.03"])
+# print(stock)
+assert stock.symbol == "MSFT"
+assert stock.date == datetime.date(2018, 12, 14)
+assert stock.closing_price == 106.03
+
+
+from typing import Optional
+import re
+
+def try_parse_row(row: List[str]) -> Optional[StockPrice]:
+    symbol, date_, closing_price_ = row
+    if not re.match(r"^[A-Z]+$", symbol):
+        return None
+    try:
+        date = parse(date_).date()
+    except ValueError:
+        return None
+    try:
+        closing_price = float(closing_price_)
+    except ValueError:
+        return None
+    return StockPrice(symbol, date, closing_price)
+
+assert try_parse_row(["MSFT0", "2018-12-14", "106.03"]) is None
+assert try_parse_row(["MSFT", "2018-12--14", "106.03"]) is None
+assert try_parse_row(["MSFT", "2018-12-14", "x"]) is None
+
+# But should return same as before if data is good.
+assert try_parse_row(["MSFT", "2018-12-14", "106.03"]) == stock
+
+import csv
+
+data: List[StockPrice] = []
+
+with open("stock.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        maybe_stock = try_parse_row(row)
+        if maybe_stock is None:
+            print(f"Skipping invalid row: {row}")
+        else:
+            data.append(maybe_stock)
+print(data)
