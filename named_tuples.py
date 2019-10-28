870fea7c04c2085b54543e4e73e2f85b448dd171
Paris <parismollochristondis@gmail.com>
2019-10-28 18:24:04 +0100

    NamedTuple class

--- named_tuples.py
+++ named_tuples.py
@@ -7,3 +7,18 @@ StockPrice = namedtuple('StockPrice',
 price = StockPrice('MSFT', datetime.date(2018, 12, 14), 106.03)
 assert price.symbol == 'MSFT'
 assert price.closing_price == 106.03
+
+from typing import NamedTuple
+
+class StockPrice(NamedTuple):
+    symbol: str
+    date: datetime.date
+    closing_price: float
+
+    def is_high_tech(self) -> bool:
+        return self.symbol in ['MSFT', 'GOOG', 'FB', 'AMZN', 'AAPL']
+
+price = StockPrice('MSFT', datetime.date(2018, 12, 14), 106.03)
+
+assert price.symbol == 'MSFT'
+assert price.closing_price == 106.03
